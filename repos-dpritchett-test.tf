a341dc6ea6aac50f4ba3ed0cbcbb841873e1532b
promoterbot <promoterbot@gremlininc.com>
2019-06-26 11:43:54 -0500

    Create a cool-hacks repo and set it to read-only for the developers team

--- repos-dpritchett-test.tf
+++ repos-dpritchett-test.tf
@@ -31,3 +31,20 @@ resource "github_branch_protection" "protect-demo-repo-master" {
   # no one can push without passing these restrictions
   restrictions {}
 }
+
+resource "github_repository" "dpritchett-test_repo_cool-hacks" {
+  provider = "github.dpritchett-test"
+
+  name        = "cool-hacks"
+  description = "Cool hacks to amaze your friends"
+
+  private = false
+}
+
+resource "github_team_repository" "dpritchett-test_team_repository_cool-hacks" {
+  provider = "github.dpritchett-test"
+
+  team_id    = "${github_team.dpritchett-test-developers.id}"
+  repository = "${github_repository.dpritchett-test_repo_cool-hacks.name}"
+  permission = "pull"
+}
